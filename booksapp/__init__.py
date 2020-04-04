12191e650e20dbe6b57edc16e5a0ca17b3b796c2
hassanjamilah <hassan.jamila@n2nconnect.com>
2020-04-04 20:38:34 +0800

    Started the app

--- booksapp/__init__.py
+++ booksapp/__init__.py
@@ -3,6 +3,13 @@ from flask_cors import CORS
 from flask_sqlalchemy import sqlalchemy
 from booksapp.models import setup_db , Book
 
+
+
+
+
+
+
+
 def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
